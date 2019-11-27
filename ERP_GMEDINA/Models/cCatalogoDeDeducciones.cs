28eea87af8f5e6c76e13da949fa3cbbd1a629c93
selvin <selvinmedina0016@gmail.com>
2019-11-27 16:10:31 -0600

    Add project files.

---
+++ ERP_GMEDINA/Models/cCatalogoDeDeducciones.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ERP_GMEDINA.Models
+{
+    [MetadataType(typeof(cCatalogoDeDeducciones))]
+    public partial class tbCatalogoDeDeducciones
+    {
+    }
+    public class cCatalogoDeDeducciones
+    {
+        [Display(Name = "ID Deducciones")]
+        public int cde_IdDeducciones { get; set; }
+
+        [Display(Name = "Descripcion Deduccion")]
+        [Required(ErrorMessage = "No puede dejar campos vacios.")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "El Campo {0} debe tener una longitud de {1}")]
+        public string cde_DescripcionDeduccion { get; set; }
+
+        [Display(Name = "Tipo Deduccion")]
+        [Required]
+        public int tde_IdTipoDedu { get; set; }
+
+        
+        [Display(Name = "Porcentaje Colaborador")]
+        [Required(ErrorMessage = "No puede dejar campos vacios.")]
+        [Range(0, 99.99, ErrorMessage = "El monto {0} debe estar entre {1} y {2}.")]
+        [RegularExpression(@"^[0-9]+(\.[0-9]{1,2})$", ErrorMessage = "Número decimal válido con un máximo de 2 decimales.")]     
+        public decimal cde_PorcentajeColaborador { get; set; }
+
+        
+        [Display(Name = "Porcentaje Empresa")]
+        [Required(ErrorMessage = "No puede dejar campos vacios.")]
+        [Range(0, 99.99, ErrorMessage = "El monto {0} debe estar entre {1} y {2}.")]
+        [RegularExpression(@"^[0-9]+(\.[0-9]{1,2})$", ErrorMessage = "Número decimal válido con un máximo de 2 decimales.")]
+        public decimal cde_PorcentajeEmpresa { get; set; }
+
+        [Display(Name = "Creado por")]
+        public int cde_UsuarioCrea { get; set; }
+
+        [Display(Name = "Fecha de Creacion")]
+        public System.DateTime cde_FechaCrea { get; set; }
+
+        [Display(Name = "Modificado por")]
+        public Nullable<int> cde_UsuarioModifica { get; set; }
+
+        [Display(Name = "Fecha Modificacion")]
+        public Nullable<System.DateTime> cde_FechaModifica { get; set; }
+
+        [Display(Name = "Activo")]
+        public bool cde_Activo { get; set; }
+    }
+}
