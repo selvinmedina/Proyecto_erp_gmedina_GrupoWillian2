7f831ec21748dedc58528b7f6061d8d7193ea1a0
selvinmedina <selvinmedina0016@gmail.com>
2019-12-03 10:11:14 -0600

    Validaciones a las fechas

--- ERP_GMEDINA/Helpers/Liquidacion.cs
+++ ERP_GMEDINA/Helpers/Liquidacion.cs
@@ -8,82 +8,9 @@ namespace ERP_GMEDINA.Helpers
 {
     public class Liquidacion
     {
-        //public static object IntervaloEntreFechas(int idEmpleado, DateTime fechaFinal, ref int sDias, ref int sMeses, ref int sAnios, out DateTime dFechaInicial)
-        //{
-        //    DateTime dFechaFinal = fechaFinal;
-        //    dFechaInicial = DateTime.MinValue;
-        //    using (ERP_GMEDINAEntities db = new ERP_GMEDINAEntities())
-        //    {
-        //        try
-        //        {
-        //            dFechaInicial = db.tbEmpleados.Where(x => x.emp_Id == idEmpleado).Select(x => x.emp_Fechaingreso).FirstOrDefault();
-        //        }
-        //        catch (Exception ex)
-        //        {
-                    
-        //        }
-        //    }
-
-        //    TimeSpan difFechas = dFechaFinal - dFechaInicial;
-            
-
-        //    if(dFechaInicial > dFechaFinal)
-        //        return new { sDias = 0, sMeses = 0, sAnios = 0};
-
-        //    double cantidadDias = Dias360Mes(dFechaInicial, dFechaFinal);
-            
-        //    int dias = 0,
-        //        meses = 0,
-        //        anios = 0,
-
-        //        //Numero de dias, numero del mes, y numero del año en de la fecha inicial
-        //        diasFechaInicio = dFechaInicial.Day,
-        //        mesFechaInicio = dFechaInicial.Month,
-        //        anioFechaInicio = dFechaInicial.Year,
-
-        //        //Numero de dias, numero del mes, y numero del año en de la fecha final
-        //        diasFechaFinal = dFechaFinal.Day,
-        //        mesFechaFinal = dFechaFinal.Month,
-        //        anioFechaFinal = dFechaFinal.Year;
-
-
-        //    if (diasFechaFinal < diasFechaInicio)
-        //    {
-                
-        //        mesFechaFinal -= 1;
-        //    }
-
-        //    //Calcula dias de antiguedad)
-
-        //    dias = (diasFechaFinal - diasFechaInicio) + 1;
-
-        //    if (mesFechaFinal < mesFechaInicio)
-        //    {
-        //        mesFechaFinal += 12;
-        //        anioFechaFinal -= 1;
-        //    }
-
-        //    //Calcula meses de antiguedad
-        //    meses = mesFechaFinal - mesFechaInicio;
-
-        //    //Calcula años de antiguedad
-        //    anios = anioFechaFinal - anioFechaInicio;
-
-        //    //Asignar los dias meses y años a las variables ref
-        //    sDias = (dias);
-        //    sMeses = meses;
-        //    sAnios = anios;
-
-        //    if (sDias < 0)
-        //        sDias += 30;
-
-        //    return new { sDias, sMeses, sAnios };
-
-        //}
-
-
         public static double Dias360Mes(DateTime fechaFin, int idEmpleado)
         {
+            #region Calcular las fechas, año de 360 días
             DateTime fechaInicio = DateTime.MinValue;
             using (ERP_GMEDINAEntities db = new ERP_GMEDINAEntities())
             {
@@ -96,6 +23,9 @@ namespace ERP_GMEDINA.Helpers
 
                 }
             }
+
+            if (fechaInicio > fechaFin)
+                return 0;
 
             int diaInicio = fechaInicio.Day;
             int mesInicio = fechaInicio.Month;
@@ -121,5 +51,6 @@ namespace ERP_GMEDINA.Helpers
         {
             return date.Month == 2 && date.Day == DateTime.DaysInMonth(date.Year, date.Month);
         }
+        #endregion
     }
 }
